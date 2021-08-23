5ef6e69d0e56bfe46a73d83f338bd04bce92b819
sadikakm <jsadika96@gmai.com>
2021-08-24 00:30:03 +0600

    upload

---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from .models import Post
+
+# Create your views here.
+
+def blog_list (request):
+    posts = Post.objects.all()
+  
+
+    context ={
+        'posts':posts
+     
+
+
+    }
+    return render (request,'blog/index.html',context)
